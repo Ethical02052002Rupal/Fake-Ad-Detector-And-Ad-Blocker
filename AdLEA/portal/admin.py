afb641105ab18526e2662e7f2a2a49c38f857926
Prince Singh <netprincesingh@gmail.com>
2026-04-27 19:13:22 +0530

    minor fix

--- AdLEA/portal/admin.py
+++ AdLEA/portal/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import BlacklistedAd
 
-# Register your models here.
+
+@admin.register(BlacklistedAd)
+class BlacklistedAdAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at', 'reviewed')
+    list_filter = ("reviewed", "created_at")
+    search_fields = ('title', 'body', 'link_url')
+    readonly_fields = ('created_at',)
+
+
